e07e8cd019afc80c7df64f5f2199352b0b288e35
iSensev2 <75488500+iSensev2@users.noreply.github.com>
2024-08-02 17:48:18 +0800

    Delete Docs directory

--- Docs/scriptResto.js
+++
@@ -1,6 +0,0 @@
-// Function to add item to cart
-function addToCart(itemName, price) {
-    // Here you can implement your logic to add items to cart
-    console.log(`Added ${itemName} to cart with price $${price}`);
-    alert(`Added ${itemName} to cart!`);
-}
